4169373b9532e12feebb34cbe899ad3b4603699e
CheezItMan <chris@adadevelopersacademy.org>
2019-06-10 10:51:50 -0700

    Add Student Component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import Student from './components/Student'
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h2>Ada Students!</h2>
+      </header>
+      <main>
+        <Student />
+        <Student />
+        <Student />
+        <Student />
+
+      </main>
 
     </div>
   );
