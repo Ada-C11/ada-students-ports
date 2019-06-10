04e72b9d3b64588149755d74890d09eac9f8ef25
dee <dee@adadevelopersacademy.org>
2019-06-10 14:02:31 -0700

    refactor student collection to render a list of student components

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -2,12 +2,33 @@ import React from 'react';
 import Student from './Student';
 
 const StudentCollection = () => {
+
+  // One day, this will not be assigned to an array literal... it will be assigned to JSON coming from an API ;)
+  const students = [
+    {
+      fullName: "Ada Lovelace",
+      email: "ada@lovelace.uk",
+    },
+    {
+      fullName: "Katherine Johnson",
+      email: "kat@nasa.gov",
+    },
+  ];
+
+  const studentComponents = students.map( (student, i) => {
+    return (
+      <li key={i}>
+        <Student
+          fullName={ student.fullName }
+          email={ student.email } />
+      </li>
+    );
+  });
+
   return (
     <section>
       <ul>
-        <li>
-          <Student fullName="Bort Sampson" email="justlikebart@bart.org" />
-        </li>
+        { studentComponents }
       </ul>
     </section>
   );
